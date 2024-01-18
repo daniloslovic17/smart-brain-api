9f895e4b5b1cb17949ec748441df2b82ccd4a80a
daniloslovic17 <daniloslovic17@gmail.com>
2024-01-18 13:22:57 +0100

    Updating entries

--- server.js
+++ server.js
@@ -104,22 +104,18 @@ app.get('/profile/:id', (req, res) => {
 			res.status(400).json('Not Found')
 		}
 	})
-	.catch( err => res.status(400).json('error getting user'))
+	.catch( err => res.status(400).json('Error getting user'))
 })
 
 app.put('/image', (req, res) => {
 	const { id } = req.body;
-	let found = false;
-	database.users.forEach(user => {
-		if(user.id === id) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
 	})
-	if(!found){
-		return res.status(404).json('no such user');
-	}
+	.catch(err => res.status(400).json('Unable to get entries'))
 })
 
 app.listen(3001, ()=>{
